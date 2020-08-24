39495cfa107b2da1059704898fb0493093cc2929
Tanish Agarwal <tanishagarwal1998@gmail.com>
2020-08-24 22:47:53 +0530

    Basic mikro-orm setup
    Set up mikro-orm to connect to our postgresql database

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import 'reflect-metadata';
+import { MikroORM } from '@mikro-orm/core';
+import { __prod__ } from './constants';
+// import { Post } from './entities/Post';
+import microConfig from './mikro-orm.config';
+// import express from 'express';
+// import { ApolloServer } from 'apollo-server-express';
+// import { buildSchema } from 'type-graphql';
+// import { PostResolver } from './resolvers/post';
+// import { UserResolver } from './resolvers/user';
+
+const main = async () => {
+	const orm = await MikroORM.init(microConfig);
+	await orm.getMigrator().up();
+
+	// //initialise express server
+	// const app = express();
+
+	// //Apollo server for graphql endpoints
+	// const apolloServer = new ApolloServer({
+	// 	schema: await buildSchema({
+	// 		resolvers: [PostResolver, UserResolver],
+	// 		validate: false,
+	// 	}),
+	// 	context: () => ({ em: orm.em.fork() }),
+	// });
+
+	// //start apolloserver
+	// apolloServer.applyMiddleware({ app });
+
+	// //Listening port
+	// app.listen(4000, () => {
+	// 	console.log('server started on localhost:4000');
+	// });
+};
+
+main().catch((err) => {
+	console.error(err);
+});
